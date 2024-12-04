22f9fdd1de8a7e602ac7f3e355d06317b3f4df6e
Павел <patsey.pavel@gmail.com>
2024-12-04 10:26:47 +0300

    added crib

--- crib.py
+++ crib.py
@@ -1,18 +1,10 @@
-def get_data(input_file):
-    with open(input_file, "r") as file:
-        data = file.read().splitlines()
-    return data
-
-
-def get_answer_1(data):
-    return
-
-
-def main():
-    data = get_data("test_input")
-    print(data)
-    print(get_answer_1(data))
-
-
-if __name__ == "__main__":
-    main()
+DIRS8 = (
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+)
