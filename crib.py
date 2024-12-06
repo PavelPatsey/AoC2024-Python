bcad65106e70c4a73577f7c3254930c43fc4a10d
Павел <patsey.pavel@gmail.com>
2024-12-06 09:19:15 +0300

    added dirs

--- crib.py
+++ crib.py
@@ -15,7 +15,12 @@ RIGHT = (0, 1)
 UP = (-1, 0)
 DOWN = (1, 0)
 
-DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
+DIRS = {
+    "UP": (-1, 0),
+    "DOWN": (1, 0),
+    "LEFT": (0, -1),
+    "RIGHT": (0, 1),
+}
 
 
 def rotate(dir):
