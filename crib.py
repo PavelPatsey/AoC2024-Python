c5b2abb6b69c8945753e67f457f32e41ba1ee9ab
Павел <patsey.pavel@gmail.com>
2024-12-06 08:16:28 +0300

    dirs and rotate

--- crib.py
+++ crib.py
@@ -8,3 +8,15 @@ DIRS8 = (
     (1, 0),
     (1, 1),
 )
+
+
+LEFT = (0, -1)
+RIGHT = (0, 1)
+UP = (-1, 0)
+DOWN = (1, 0)
+
+DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
+
+
+def rotate(dir):
+    return dir[0], -dir[1]
