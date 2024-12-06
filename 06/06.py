c485f9da7209929ae757f361c9888ed8f9af44f3
Павел <patsey.pavel@gmail.com>
2024-12-06 08:44:10 +0300

    solved day 6 part 1

--- 06/06.py
+++ 06/06.py
@@ -7,7 +7,7 @@ DIRS = {
 
 
 def rotate(dir):
-    return dir[0], -dir[1]
+    return dir[1], -dir[0]
 
 
 def get_matrix(input_file):
@@ -17,13 +17,32 @@ def get_matrix(input_file):
     return matrix
 
 
-def get_answer(data):
-    return
+def get_answer(matrix):
+    ROWS = len(matrix)
+    COLS = len(matrix[0])
+    start = None
+    for r in range(ROWS):
+        for c in range(COLS):
+            if matrix[r][c] == "^":
+                start = r, c
+                break
+
+    r, c = start
+    visited = set()
+    visited.add(start)
+    dir = DIRS["UP"]
+    while 0 < r < ROWS - 1 and 0 < c < COLS - 1:
+        new = r + dir[0], c + dir[1]
+        while matrix[new[0]][new[1]] == "#":
+            dir = rotate(dir)
+            new = r + dir[0], c + dir[1]
+        visited.add(new)
+        r, c = new
+    return len(visited)
 
 
 def main():
-    matrix = get_matrix("test_input")
-    print(matrix)
+    matrix = get_matrix("input")
     print(get_answer(matrix))
 
 
