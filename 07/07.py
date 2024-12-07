2296eddd4859ca386b9570cb31f5c20865678cf4
Павел <patsey.pavel@gmail.com>
2024-12-07 08:36:00 +0300

    solved day 07 part 2

--- 07/07.py
+++ 07/07.py
@@ -28,12 +28,35 @@ def is_possible(rule):
     return _is_possible(val, nums, 0, nums[0])
 
 
+def is_possible_2(rule):
+
+    def _is_possible_2(value, numbers, i, summa):
+        if i == len(numbers) - 1:
+            return value == summa
+        a = summa + numbers[i + 1]
+        b = summa * numbers[i + 1]
+        c = int(str(summa) + str(numbers[i + 1]))
+        return any(
+            (
+                _is_possible_2(value, numbers, i + 1, a),
+                _is_possible_2(value, numbers, i + 1, b),
+                _is_possible_2(value, numbers, i + 1, c),
+            )
+        )
+
+    val, nums = rule
+    return _is_possible_2(val, nums, 0, nums[0])
+
+
 def get_answer(rules):
     res = 0
+    res_2 = 0
     for rule in rules:
         if is_possible(rule):
             res += rule[0]
-    return res
+        if is_possible_2(rule):
+            res_2 += rule[0]
+    return res, res_2
 
 
 def main():
