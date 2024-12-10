40a4690a3c5bb80a1f0b9afcd938ba544d3a4223
Павел <patsey.pavel@gmail.com>
2024-12-10 09:54:44 +0300

    solved day 10 part 2

--- 10/10.py
+++ 10/10.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 DIRS4 = (
     (0, -1),
     (0, 1),
@@ -58,10 +60,39 @@ def get_score(grid, start):
     return len(peaks)
 
 
+def get_score_2(grid, start):
+    peaks = defaultdict(int)
+
+    def dfs(r, c, prev_value, visited):
+        if not in_grid(r, c, grid):
+            return
+        if (r, c) in visited:
+            return
+        if grid[r][c] != prev_value + 1:
+            return
+        if grid[r][c] == 9:
+            node = r, c
+            peaks[node] += 1
+            return
+
+        for new_dir in DIRS4:
+            dr, dc = new_dir
+            new_r = r + dr
+            new_c = c + dc
+            visited_node = r, c
+            new_visited = visited | {visited_node}
+            value = grid[r][c]
+            dfs(new_r, new_c, value, new_visited)
+
+    dfs(start[0], start[1], -1, set())
+    return sum(peaks.values())
+
+
 def main():
     grid = get_grid("input")
     starts = get_starts(grid)
     print(sum(get_score(grid, node) for node in starts))
+    print(sum(get_score_2(grid, node) for node in starts))
 
 
 if __name__ == "__main__":
