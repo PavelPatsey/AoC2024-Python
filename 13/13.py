8903764c23a1936b24cc810a4f60a352bb0b46d3
Павел <patsey.pavel@gmail.com>
2024-12-13 11:46:11 +0300

    cosmetic changes

--- 13/13.py
+++ 13/13.py
@@ -23,10 +23,15 @@ def get_machines(input_file):
     return machines
 
 
-def f(a, b, p):
-    x1, y1 = a
-    x2, y2 = b
-    x, y = p
+def solve(mach_a, mach_b, prize):
+    """
+    a * x1 + b * x2 = x
+    a * y1 + b * y2 = y
+    """
+    x1, y1 = mach_a
+    x2, y2 = mach_b
+    x, y = prize
+
     c2 = y2 * x1 - x2 * y1
     if c2 == 0:
         return
@@ -34,17 +39,17 @@ def f(a, b, p):
     if not c1 % c2 == 0:
         return
     b = c1 // c2
-    if not (x - b * x2) % x1 == 0:
+    c3 = x - b * x2
+    if not c3 % x1 == 0:
         return
-    a = (x - b * x2) // x1
+    a = c3 // x1
     return a, b
 
 
 def get_answer(machines):
     res = 0
-    for m in machines:
-        a, b, p = m
-        sol = f(a, b, p)
+    for a, b, p in machines:
+        sol = solve(a, b, p)
         if sol:
             res += sol[0] * A_PRICE + sol[1] * B_PRICE
     return res
@@ -52,10 +57,9 @@ def get_answer(machines):
 
 def get_answer_2(machines):
     res = 0
-    for m in machines:
-        a, b, p = m
+    for a, b, p in machines:
         p2 = p[0] + ADDITION, p[1] + ADDITION
-        sol = f(a, b, p2)
+        sol = solve(a, b, p2)
         if sol:
             res += sol[0] * A_PRICE + sol[1] * B_PRICE
     return res
