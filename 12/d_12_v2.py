8010e23fc0e23323645df9cb0221e1cec3af04d5
Павел <patsey.pavel@gmail.com>
2024-12-13 15:01:04 +0300

    solved part 2 in other way

--- 12/d_12_v2.py
+++ 12/d_12_v2.py
@@ -1,56 +1,102 @@
-from collections import defaultdict
-from functools import reduce
-from itertools import groupby
-from operator import itemgetter
-
-DIRS4 = (
-    (0, -1),
-    (0, 1),
-    (-1, 0),
-    (1, 0),
-)
-
-DIRS4_dict = {
-    "UP": (-1, 0),
-    "DOWN": (1, 0),
-    "LEFT": (0, -1),
-    "RIGHT": (0, 1),
-}
+from day_12 import DIRS4, get_grid, get_perimeter, get_plots_bfs
 
 
-def get_grid(input_file):
-    with open(input_file, "r") as file:
-        data = file.read().splitlines()
-    return [[x for x in line] for line in data]
+def rotate_clockwise(dir):
+    return dir[1], -dir[0]
 
 
-def in_grid(r, c, grid):
-    rows = len(grid)
-    cols = len(grid[0])
-    return 0 <= r < rows and 0 <= c < cols
+def rotate_counterclockwise(dir):
+    return -dir[1], dir[0]
 
 
+def get_half_of_corners_count(plot, dir, plot_set):
+    """
+    Рассмотрим ноду А, и ноды: сверху, справа, по диагонали
+    0) если есть верхняя нода, тогда нет углов
+    1) если все ноды не входят в сет, тогда будет угол
 
+    _.
+    A|
+
+    2) если диагональный элемент в сете, тогда будет угол
+
+    01  _#  _#
+    А2  A#  A|
+
+    если есть угол добавляем 1/2, так как он будет рассмотрен еще раз,
+    потому что один угол принадлежит двум сторонам
+    """
+    r, c = plot
+    dr, dc = dir
+    r0, c0 = r + dr, c + dc
+
+    if (r0, c0) in plot_set:
+        return 0
+
+    res = 0
+    for dir2 in (rotate_clockwise(dir), rotate_clockwise(dir)):
+        dr2, dc2 = dir2
+        r2, c2 = r + dr2, c + dc2
+        r1, c1 = r + dr + dr2, c + dc + dc2
+        nodes = ((r0, c0), (r1, c1), (r2, c2))
+        mapped = map(lambda x: x in plot_set, nodes)
+        if sum(mapped) == 0 or (r1, c1) in plot_set:
+            res += 1 / 2
+
+    return res
 
 
 def get_corners_number(plot_set):
-
-    return 0
+    res = 0
+    for plot in plot_set:
+        for dir in DIRS4:
+            res += get_half_of_corners_count(plot, dir, plot_set)
+    return int(res)
 
 
 def main():
-    grid = get_grid("input")
+    file = "input"
+    grid = get_grid(file)
 
-    plots_bfs = get_plots_bfs(grid)
-    ans1_bfs = sum(map(lambda x: len(x) * get_perimeter(grid, x), plots_bfs.values()))
+    plots = get_plots_bfs(grid)
+    ans1_bfs = sum(map(lambda x: len(x) * get_perimeter(grid, x), plots.values()))
     print(ans1_bfs)
 
-    ans2 = sum(map(lambda x: len(x) * get_corners_number(x), plots_bfs.values()))
+    ans2 = sum(map(lambda x: len(x) * get_corners_number(x), plots.values()))
     print(ans2)
 
-    assert ans1_bfs == 1424006
-    assert ans2 == 858684
+    if file == "test_input_1":
+        assert ans1_bfs == 140
+        assert ans2 == 80
+    elif file == "test_input_2":
+        assert ans1_bfs == 772
+        assert ans2 == 436
+    elif file == "test_input_larger":
+        assert ans1_bfs == 1930
+        assert ans2 == 1206
+    elif file == "input":
+        assert ans1_bfs == 1424006
+        assert ans2 == 858684
+    else:
+        raise Exception("unknown input!")
 
 
 if __name__ == "__main__":
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+    UP = (-1, 0)
+    DOWN = (1, 0)
+
+    assert rotate_clockwise(LEFT) == UP
+    assert rotate_clockwise(UP) == RIGHT
+    assert rotate_clockwise(RIGHT) == DOWN
+    assert rotate_clockwise(DOWN) == LEFT
+
+    assert rotate_counterclockwise(LEFT) == DOWN
+    assert rotate_counterclockwise(DOWN) == RIGHT
+    assert rotate_counterclockwise(RIGHT) == UP
+    assert rotate_counterclockwise(UP) == LEFT
+
+    assert get_corners_number({(2, 3), (1, 2), (3, 3), (2, 2)}) == 8
+
     main()
