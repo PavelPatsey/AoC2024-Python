ff983439dee8d55aaeea4b7dbaff3e5c240d93f9
Павел <patsey.pavel@gmail.com>
2024-12-13 09:16:33 +0300

    solved day 13 part 1

--- 13/13.py
+++ 13/13.py
@@ -1,5 +1,8 @@
 import re
 
+A_PRICE = 3
+B_PRICE = 1
+
 
 def get_machines(input_file):
     def _get_machine(s):
@@ -19,14 +22,36 @@ def get_machines(input_file):
     return machines
 
 
-def get_answer(data):
-    return
+def f(machine):
+    a, b, p = machine
+    x1, y1 = a
+    x2, y2 = b
+    x, y = p
+    c2 = y2 * x1 - x2 * y1
+    if c2 == 0:
+        return
+    c1 = y * x1 - x * y1
+    if not c1 % c2 == 0:
+        return
+    b = c1 // c2
+    if not (x - b * x2) % x1 == 0:
+        return
+    a = (x - b * x2) // x1
+    return a, b
+
+
+def get_answer(machines):
+    res = 0
+    for m in machines:
+        sol = f(m)
+        if sol:
+            res += sol[0] * A_PRICE + sol[1] * B_PRICE
+    return res
 
 
 def main():
-    machines = get_machines("test_input")
-    print(machines)
-    # print(get_answer(machines))
+    machines = get_machines("input")
+    print(get_answer(machines))
 
 
 if __name__ == "__main__":
