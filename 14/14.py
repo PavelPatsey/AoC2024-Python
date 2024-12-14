23bbe35010f9c92664e504121a24c40d050ba601
Павел <patsey.pavel@gmail.com>
2024-12-14 13:08:32 +0300

    solved day 14 part 2

--- 14/14.py
+++ 14/14.py
@@ -1,8 +1,15 @@
 import re
-from collections import Counter
+from collections import Counter, defaultdict
 from copy import deepcopy
 from functools import reduce
 from operator import mul
+
+DIRS4 = (
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (1, 0),
+)
 
 
 def get_robots(input_file):
@@ -45,14 +52,89 @@ def get_answer(robots):
     return reduce(mul, counter.values())
 
 
+def get_grid(coords_set):
+    grid = [[" "] * COLS for _ in range(ROWS)]
+    for r in range(ROWS):
+        for c in range(COLS):
+            if (c, r) in coords_set:
+                grid[r][c] = "#"
+    return grid
+
+
+def print_grid(grid):
+    for row in grid:
+        print("".join(row))
+
+
+def in_grid(c, r):
+    return 0 <= r < ROWS and 0 <= c < COLS
+
+
+def get_robots_coords(robots):
+    return {r[0] for r in robots}
+
+
+def get_groups(robots):
+    visited_nodes = set()
+    groups = defaultdict(set)
+    robots_coords = get_robots_coords(robots)
+
+    def bfs(node):
+        visited = {node}
+        queue = [node]
+        while queue:
+            c, r = queue.pop()
+            for dc, dr in DIRS4:
+                nc, nr = c + dc, r + dr
+                if (
+                    in_grid(nc, nr)
+                    and (nc, nr) not in visited
+                    and (nc, nr) in robots_coords
+                ):
+                    queue.append((nc, nr))
+                    visited.add((nc, nr))
+        return visited
+
+    for _r in range(ROWS):
+        for _c in range(COLS):
+            if (_c, _r) not in visited_nodes:
+                plot_set = bfs((_c, _r))
+                groups[(_c, _r)] = plot_set
+                visited_nodes.update(plot_set)
+
+    return groups
+
+
+def get_answer_2(robots):
+    converted_robots = deepcopy(robots)
+    for i in range(1, N_MAX):
+        if i % 500 == 0:
+            print(f"{i=}")
+        converted_robots = [convert_robot(r) for r in converted_robots]
+        groups = get_groups(converted_robots)
+        # g_lens = (len(g) for g in groups.values())
+        m_g_lens = max(len(g) for g in groups.values())
+        if m_g_lens >= MAX_GROUP_LEN:
+            coords_set = get_robots_coords(converted_robots)
+            grid = get_grid(coords_set)
+            print_grid(grid)
+            print(f"{i=} {m_g_lens=}")
+            return i
+    return
+
+
 # ROWS, COLS = 7, 11
 ROWS, COLS = 103, 101
 FILE = "input"
+
+N_MAX = 10_000
+MAX_GROUP_LEN = 200
 
 
 def main():
     robots = get_robots(FILE)
     print(get_answer(robots))
+    print(get_answer_2(robots))
 
 
 if __name__ == "__main__":
