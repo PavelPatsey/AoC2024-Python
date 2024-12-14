599d5cb294a8d69f80d2e586bd5e7f0fe7eb4d6d
Павел <patsey.pavel@gmail.com>
2024-12-14 11:28:57 +0300

    cosmetic changes

--- 14/14.py
+++ 14/14.py
@@ -7,19 +7,14 @@ from operator import mul
 
 def get_robots(input_file):
     def _get_robot(string):
-        "p=0,4 v=3,-3"
-        pattern = re.compile(r"p=(-?\d+),(-?\d+)\s+v=(-?\d+),(-?\d+)")
+        pattern = re.compile(r"-?\d+")
         matches = pattern.findall(string)
-        px, py, vx, vy = map(int, matches[0])
+        px, py, vx, vy = map(int, matches)
         return (px, py), (vx, vy)
 
     with open(input_file, "r") as file:
         data = file.read().splitlines()
     return [_get_robot(line) for line in data]
-
-
-def get_grid():
-    return [[0] * COLS for _ in range(ROWS)]
 
 
 def convert_robot(robot):
@@ -37,8 +32,6 @@ def get_quadrant(robot):
     cols_mid = COLS // 2
     if r == rows_mid or c == cols_mid:
         return None
-
-    res = c < cols_mid, r < rows_mid
     return c < cols_mid, r < rows_mid
 
 
@@ -52,8 +45,8 @@ def get_answer(robots):
     return reduce(mul, counter.values())
 
 
-# ROWS, COLS = 7, 11  # 7, 11 or 103, 101
-ROWS, COLS = 103, 101  # 7, 11 or 103, 101
+# ROWS, COLS = 7, 11
+ROWS, COLS = 103, 101
 FILE = "input"
 
 
