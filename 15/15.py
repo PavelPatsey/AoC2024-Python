3b0a89167c7675e1c6bd229233efa5acfe681a2b
Павел <patsey.pavel@gmail.com>
2024-12-15 09:10:34 +0300

    added do_move function

--- 15/15.py
+++ 15/15.py
@@ -1,19 +1,85 @@
+from copy import deepcopy
+
+DIRS = {
+    "<": (0, -1),
+    ">": (0, 1),
+    "^": (-1, 0),
+    "v": (1, 0),
+}
+
+
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().strip()
     grid, moves = data.split("\n\n")
     grid = grid.split()
+    grid = [[x for x in line] for line in grid]
     return grid, moves
 
 
-def get_answer(grid, moves):
+def print_grid(grid):
+    for row in grid:
+        print("".join(row))
+    print()
     return
+
+
+def gets_start(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "@":
+                return r, c
+    return
+
+
+def convert(grid, move, node):
+    dir = DIRS[move]
+
+    def do_move(pos):
+        r, c = pos
+        dr, dc = dir
+        nr, nc = r + dr, c + dc
+        new_pos = nr, nc
+        if grid[nr][nc] == ".":
+            grid[r][c], grid[nr][nc] = grid[nr][nc], grid[r][c]
+            return True
+        elif grid[nr][nc] == "O":
+            moved = do_move(new_pos)
+            if moved:
+                grid[r][c], grid[nr][nc] = grid[nr][nc], grid[r][c]
+                return True
+            return False
+        elif grid[nr][nc] == "#":
+            return False
+        else:
+            raise Exception("invalid case!")
+
+    do_move(node)
+    new_start = gets_start(grid)
+
+    return grid, new_start
+
+
+def get_answer(grid, moves, start):
+    node = start
+    converted_grid = deepcopy(grid)
+    for move in moves:
+        converted_grid, node = convert(grid, move, node)
+        print(f"Move {move}:")
+        print_grid(grid)
 
 
 def main():
     grid, moves = get_data("test_input.txt")
-    print(grid, moves)
-    print(get_answer(grid, moves))
+    print("Initial state:")
+    print_grid(grid)
+
+    start = gets_start(grid)
+
+    ans1 = get_answer(grid, moves, start)
+    print(f"{ans1=}")
 
 
 if __name__ == "__main__":
