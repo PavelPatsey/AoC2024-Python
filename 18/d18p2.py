0292af8ed30eefd7e5a4cbce6ae04b4cb1ac49ec
Павел <patsey.pavel@gmail.com>
2024-12-18 09:33:44 +0300

    no need in heapq

--- 18/d18p2.py
+++ 18/d18p2.py
@@ -36,43 +36,45 @@ def print_grid(grid):
 
 
 def get_answer(n, m, bytes):
+    def bfs():
+        start = 0, 0
+        end = n, n
+
+        sr, sc = start
+        visited = set()
+        queue = deque()
+        queue.append((0, sr, sc))
+
+        while queue:
+            score, r, c = queue.popleft()
+            if (r, c) == end:
+                return score
+            for dr, dc in DIRS4:
+                nr, nc = r + dr, c + dc
+                new_score = score + 1
+                if (
+                    in_grid(nr, nc, grid)
+                    and grid[nr][nc] != "#"
+                    and (nr, nc) not in visited
+                ):
+                    queue.append((new_score, nr, nc))
+                    visited.add((nr, nc))
+
+        return
+
     bytes_slice = bytes[0:m]
     grid = get_grid(n, bytes_slice)
 
-    start = 0, 0
-    end = n, n
-
-    sr, sc = start
-    visited = set()
-    queue = deque()
-    queue.append((0, sr, sc))
-
-    while queue:
-        score, r, c = queue.popleft()
-        if (r, c) == end:
-            return score
-        for dr, dc in DIRS4:
-            nr, nc = r + dr, c + dc
-            new_score = score + 1
-            if (
-                in_grid(nr, nc, grid)
-                and grid[nr][nc] != "#"
-                and (nr, nc) not in visited
-            ):
-                queue.append((new_score, nr, nc))
-                visited.add((nr, nc))
-
-    return
-
 
 def main():
-    # n = 6
-    # m = 12
-    # file = "test_input.txt"
+    n = 6
+    m = 12
+    file = "test_input.txt"
 
-    n = 70
-    m = 1024
-    file = "input.txt"
+    # n = 70
+    # m = 1024
+    # file = "input.txt"
+
     bytes = get_data(file)
     print(get_answer(n, m, bytes))
 
