a31e28804a98612df0efb78442b1b2d07319ea0a
Павел <patsey.pavel@gmail.com>
2024-12-21 13:13:25 +0300

    passed test case

--- 21/d21p1.py
+++ 21/d21p1.py
@@ -1,5 +1,6 @@
 from collections import deque
 from functools import cache
+from typing import List, Set
 
 DIRS4_dict = {
     "^": (-1, 0),
@@ -61,12 +62,11 @@ def get_dist(start, end):
 
 
 @cache
-def dfs(start, char, keypad):
+def dfs(start, char, keypad) -> List:
     """Получить последовательности соответствующие Manhattan distance"""
-    sr, sc = start
     end = get_end(char, keypad)
     max_dist = get_dist(start, end)
-    all_seqs = set()
+    all_seqs = []
 
     def _dfs(r, c, seq):
         if len(seq) > max_dist:
@@ -77,7 +77,7 @@ def dfs(start, char, keypad):
             return
         if keypad[r][c] == char:
             assert len(seq) == max_dist
-            all_seqs.add(seq + "A")
+            all_seqs.append(seq + "A")
         for key, drt in DIRS4_dict.items():
             dr, dc = drt
             nr, nc = r + dr, c + dc
@@ -102,19 +102,19 @@ def get_len_without_duplicates(seq):
     # print(f"{mapped=}")
 
 
-def convert_char(start, char, keypad):
+def convert_char(start, char, keypad) -> Set:
     """Получить последовательности"""
-    all_seqs, end = dfs(start, char, keypad)
+    all_seqs_lst, end = dfs(start, char, keypad)
+    all_seqs_set = set(all_seqs_lst)
+    return all_seqs_set, end
 
-    return all_seqs, end
 
-
-def convert_seq(seq, keypad):
+def convert_seq(seq, keypad) -> Set:
     """Из одной последовательности получить возможные последовательности"""
     start = get_start(keypad)
     converted_seqs = {""}
     for char in seq:
-        all_char_seqs, start = dfs(start, char, keypad)
+        all_char_seqs, start = convert_char(start, char, keypad)
         new_converted_seq = set()
         for seq in converted_seqs:
             for char_seq in all_char_seqs:
@@ -124,42 +124,41 @@ def convert_seq(seq, keypad):
 
 
 def convert_code(code):
-    code = "029A"
-    print(f"{code=}")
+    # print(f"{code=}")
 
-    converted_1 = convert_seq(code, NUM_KP)
-    print(f"{converted_1=} {len(converted_1)=}")
-    assert len(converted_1) == len("<A^A>^^AvvvA")
+    conv_seqs_1 = convert_seq(code, NUM_KP)
+    # print(f"{conv_seqs_1=}")
 
-    converted_2 = convert_seq(converted_1, DIR_KP)
-    print(f"{converted_2=} {len(converted_2)=}")
-    assert len(converted_2) == len("v<<A>>^A<A>AvA<^AA>A<vAAA>^A")
+    conv_seqs_2 = set()
+    for seq_1 in conv_seqs_1:
+        conv_seqs_2.update(convert_seq(seq_1, DIR_KP))
+    # print(f"{conv_seqs_2=}")
+    # assert "v<<A>>^A<A>AvA<^AA>A<vAAA>^A" in conv_seqs_2
 
-    converted_3 = convert_seq(converted_2, DIR_KP)
-    print(f"{converted_3=} {len(converted_3)=}")
+    conv_seqs_3 = set()
+    for seq_2 in conv_seqs_2:
+        conv_seqs_3.update(convert_seq(seq_2, DIR_KP))
+    # print(f"{conv_seqs_3=}")
+    # assert (
+    #     "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
+    #     in conv_seqs_3
+    # )
 
-    doljno_byt = len(
-        "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
-    )
-    # a 'v<A<AA>^>AvA^<Av>A^Av<<A>^>AvA^Av<<A>^>AAv<A>A^A<A>Av<A<A>^>AAA<Av>A^A'
-    print(f"{doljno_byt=}")
-    assert len(converted_3) == len(
-        "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
-    )
+    min_len = float("inf")
+    for seq_3 in conv_seqs_3:
+        min_len = min(min_len, len(seq_3))
+
+    # print(f"{min_len=}")
 
     code_num = int(code[:-1])
-    return code_num * len(converted_3)
-
-
-def get_answer(data):
-    return
+    return code_num * min_len
 
 
 def main():
     file = "test_input.txt"
     codes = get_data(file)
-    # print(codes)
-    print(get_answer(codes))
+    ans1 = sum(map(convert_code, codes))
+    print(f"{ans1=}")
 
 
 if __name__ == "__main__":
@@ -169,9 +168,9 @@ if __name__ == "__main__":
     assert get_len_without_duplicates("1233") == 3
     assert get_len_without_duplicates("A^A^>^AvvvA") == 9
 
-    assert dfs(get_start(NUM_KP), "0", NUM_KP) == ({"<A"}, (3, 1))
-    assert dfs(get_start(NUM_KP), "A", NUM_KP) == ({"A"}, (3, 2))
-    assert dfs(get_start(DIR_KP), "<", DIR_KP) == ({"v<<A", "<v<A"}, (1, 0))
+    assert convert_char(get_start(NUM_KP), "0", NUM_KP) == ({"<A"}, (3, 1))
+    assert convert_char(get_start(NUM_KP), "A", NUM_KP) == ({"A"}, (3, 2))
+    assert convert_char(get_start(DIR_KP), "<", DIR_KP) == ({"v<<A", "<v<A"}, (1, 0))
 
     assert convert_seq("029A", NUM_KP) == {
         "<A^A>^^AvvvA",
@@ -179,11 +178,6 @@ if __name__ == "__main__":
         "<A^A^^>AvvvA",
     }
 
-    # conv_seq_2 = convert_seq("v<<A>>^A<A>AvA<^AA>A<vAAA>^A", DIR_KP)
-    # test_res = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
-    # assert len(conv_seq_2) == len(test_res)
+    assert convert_code("029A") == 68 * 29
 
-    # print(convert_code("029A"))
-    # print(68 * 29)
-    # assert convert_code("029A") == 68 * 29
-    # main()
+    main()
