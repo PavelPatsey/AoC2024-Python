bf91866ef1b8aa41d4eeeb023ca799921ab9bb3d
Павел <patsey.pavel@gmail.com>
2024-12-21 09:40:50 +0300

    added and tested convert func

--- 21/d21p1.py
+++ 21/d21p1.py
@@ -1,4 +1,5 @@
 from collections import deque
+from functools import cache
 
 DIRS4_dict = {
     "^": (-1, 0),
@@ -41,17 +42,17 @@ def get_start(keypad):
         raise Exception("Invalid keypad!")
 
 
-def bfs(start, end_symb, keypad):
+def bfs(start, char, keypad):
     sr, sc = start
     visited = {start}
     queue = deque([(sr, sc, "")])
 
     while queue:
         r, c, track = queue.popleft()
-        if keypad[r][c] == end_symb:
+        if keypad[r][c] == char:
             # t_a = track + "A"
             # print(f"{t_a =}")
-            return track + "A"
+            return track + "A", (r, c)
 
         for key, drt in DIRS4_dict.items():
             dr, dc = drt
@@ -69,8 +70,12 @@ def bfs(start, end_symb, keypad):
 
 
 def convert(seq, keypad):
-    sr, sc = get_start(keypad)
-    pass
+    new_start = get_start(keypad)
+    converted_seq = ""
+    for char in seq:
+        converted_char, new_start = bfs(new_start, char, keypad)
+        converted_seq += converted_char
+    return converted_seq
 
 
 def get_answer(data):
@@ -85,9 +90,16 @@ def main():
 
 
 if __name__ == "__main__":
-    assert bfs(get_start(NUM_KP), "0", NUM_KP) == "<A"
-    assert bfs(get_start(NUM_KP), "A", NUM_KP) == "A"
+    assert bfs(get_start(NUM_KP), "0", NUM_KP) == ("<A", (3, 1))
+    assert bfs(get_start(NUM_KP), "A", NUM_KP) == ("A", (3, 2))
 
-    assert bfs(get_start(DIR_KP), "<", DIR_KP) == "v<<A"
+    assert bfs(get_start(DIR_KP), "<", DIR_KP) == ("v<<A", (1, 0))
+
+    conv_seq = convert("029A", NUM_KP)
+    assert conv_seq in {"<A^A>^^AvvvA", "<A^A^>^AvvvA", "<A^A^^>AvvvA"}
+
+    conv_seq_2 = convert("v<<A>>^A<A>AvA<^AA>A<vAAA>^A", DIR_KP)
+    test_res = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
+    assert len(conv_seq_2) == len(test_res)
 
     main()
