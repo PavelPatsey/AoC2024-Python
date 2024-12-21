2b9e1a94ac4dccd6ae67fe61f2726d6ea7b233d6
Павел <patsey.pavel@gmail.com>
2024-12-21 08:47:53 +0300

    preparation

--- 21/d21p1.py
+++ 21/d21p1.py
@@ -1,7 +1,35 @@
+NUM_KP = [
+    "789",
+    "456",
+    "123",
+    "#0A",
+]
+
+DIR_KP = [
+    "#^A",
+    "<v>",
+]
+
+START_POSE = {
+    NUM_KP: (3, 2),
+    DIR_KP: (0, 0),
+}
+
+
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().splitlines()
     return data
+
+
+def in_keypad(r, c, keypad):
+    rows = len(keypad)
+    cols = len(keypad[0])
+    return 0 <= r < rows and 0 <= c < cols
+
+
+def find(seq, keypad):
+    pass
 
 
 def get_answer(data):
