7f801226b697cf55cdb5335fd43f28ed85c88611
Павел <patsey.pavel@gmail.com>
2024-12-23 11:12:07 +0300

    refactored

--- 23/d23p1.py
+++ 23/d23p1.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import reduce
 
 
 def get_network(input_file):
@@ -14,22 +15,19 @@ def get_network(input_file):
 
 def get_answer(network):
     cons_3 = []
-    for node_1, nbrs_1 in network.items():
-        set_1 = {node_1}
+    for node_1 in network.keys():
         for node_2 in network[node_1]:
-            set_2 = set_1 | {node_2}
             for node_3 in network[node_2]:
                 if node_3 in network[node_1]:
-                    set_3 = set_2 | {node_3}
+                    set_3 = {node_1, node_2, node_3}
                     if len(set_3) == 3 and set_3 not in cons_3:
                         cons_3.append(set_3)
 
-    filtered = []
-    for nodes in cons_3:
-        if any(map(lambda x: x.startswith("t"), nodes)):
-            filtered.append(nodes)
-
-    return len(filtered)
+    filtered = (
+        nodes for nodes in cons_3 if any(map(lambda x: x.startswith("t"), nodes))
+    )
+    filtered_len = reduce(lambda acc, x: acc + 1, filtered, 0)
+    return filtered_len
 
 
 def main():
