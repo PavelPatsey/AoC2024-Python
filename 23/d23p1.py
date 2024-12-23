baa22c9496d854f62032c0c00fc60346b3ae5857
Павел <patsey.pavel@gmail.com>
2024-12-23 09:39:48 +0300

    passed test case

--- 23/d23p1.py
+++ 23/d23p1.py
@@ -13,14 +13,41 @@ def get_connections(input_file):
 
 
 def get_answer(connections):
-    pass
+    cons_3 = []
+    for node_1, nbrs_1 in connections.items():
+        set_1 = {node_1}
+        for node_2 in connections[node_1]:
+            set_2 = set_1 | {node_2}
+            for node_3 in connections[node_2]:
+                if node_3 in connections[node_1]:
+                    set_3 = set_2 | {node_3}
+                    if len(set_3) == 3 and set_3 not in cons_3:
+                        cons_3.append(set_3)
+
+    print(cons_3)
+    print(len(cons_3))
+
+    for n1, n2, n3 in cons_3:
+        assert n1 in connections[n2] and n1 in connections[n3]
+        assert n2 in connections[n1] and n1 in connections[n3]
+        assert n3 in connections[n1] and n1 in connections[n2]
+
+    filtered = []
+    for nodes in cons_3:
+        if any(map(lambda x: x.startswith("t"), nodes)):
+            filtered.append(nodes)
+
+    print(filtered)
+    print(len(filtered))
+    return len(filtered)
 
 
 def main():
     file = "test_input.txt"
     connections = get_connections(file)
     print(connections)
-    print(get_answer(connections))
+    ans1 = get_answer(connections)
+    print(f"{ans1=}")
 
 
 if __name__ == "__main__":
