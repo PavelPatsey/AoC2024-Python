45702525d0fa79f8d3fef6c64888073adc8d0cd1
Павел <patsey.pavel@gmail.com>
2024-12-25 08:53:59 +0300

    passed test case

--- 25/d25p1.py
+++ 25/d25p1.py
@@ -30,17 +30,28 @@ def made_locks_keys(items):
     return locks, keys
 
 
-def get_answer():
-    pass
+def is_overlapped(tuple_1, tuple_2, max_value):
+    if not len(tuple_1) == len(tuple_2):
+        raise Exception("the lengths do not match!")
+    mapped = map(lambda x: x[0] + x[1] > max_value, zip(tuple_1, tuple_2))
+    return any(mapped)
+
+
+def answer(locks, keys):
+    counter = 0
+    for l in locks:
+        for k in keys:
+            if not is_overlapped(l, k, 5):
+                counter += 1
+    return counter
 
 
 def main():
     file = "test_input.txt"
     items = get_items(file)
-    print(items)
     locks, keys = made_locks_keys(items)
-    print(f"{locks=}")
-    print(f"{keys=}")
+    asn1 = answer(locks, keys)
+    print(f"{asn1=}")
 
 
 if __name__ == "__main__":
